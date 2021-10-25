dba906614e3263e85be3061f6d913828c6371b19
Predakor <86599904+Predakor@users.noreply.github.com>
2021-10-26 00:00:27 +0200

    almost work add history

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const towers = document.getElementsByClassName("tower");
+const counter = document.getElementById("counter");
 const colors = [
   "#f72585",
   "#b5179e",
@@ -11,9 +12,10 @@ const colors = [
   "#4cc9f0",
 ];
 let blocks = [];
-let currentBlock, moves, size;
+let currentBlock, moves, size, minMoves;
 
 //=================creating section======================//
+
 function start(n) {
   currentBlock = 0;
   moves = 0;
@@ -28,32 +30,44 @@ function generate(n, start) {
   for (let i = start; i < n; i++) {
     let block = document.createElement("div");
     block.className = "block";
-    block.style.width = 30 + (n - 1) * i + "%";
+    block.style.width = 30 + 8 * i + "%";
     block.style.background = colors[i];
     block.addEventListener("click", grabBlock);
     towers[0].appendChild(block);
-    blocks[i] = block;
+    blocks.push(block);
   }
   size = blocks.length;
+  updateMovesCounter();
 }
 
 function reset() {
   currentBlock = 0;
   moves = 0;
+  updateMovesCounter();
   for (let i = 0; i < size; i++) {
     towers[0].appendChild(blocks[i]);
   }
 }
 
 function change() {
-  num = hanoiNum.value;
-  if (num == size) reset();
-  else if (num < size) {
+  num = parseInt(hanoiNum.value);
+
+  if (num == size) {
+    reset();
+    return;
+  }
+
+  //desired size < actual size
+  if (num < size) {
     for (let i = num; i < size; i++) {
       blocks[i].remove();
     }
     blocks.splice(num, size - num);
-  } else generate(num, size);
+    size = blocks.length;
+  } else {
+    generate(num, size);
+  }
+  reset();
 }
 
 //=================moving section======================//
@@ -74,16 +88,31 @@ function check() {
     target.prepend(currentBlock);
     moves++;
     currentBlock = "";
-
-    if (towers[2].childNodes == size) alert("You win");
+    counter.innerText = moves + "/" + minMoves;
+    if (towers[2].childNodes.length == size) alert("You win");
   }
 }
 
+function updateMovesCounter() {
+  minMoves = 2 ** size - 1;
+  counter.innerText = "0/" + minMoves;
+}
 //=================solution section======================//
 function solution() {
+  minMoves = size * size - 1;
+  let moves = ["AB", "AC", "BC"];
+  let moves2 = ["AC", "AB", "BC"];
   if (size % 2 == 0) {
+    for (let i = 0, j = 0; i < size; i++, j++) {
+      console.log(moves[j]);
+      if (j == 2) j = 0;
+    }
     //algorith 1 AB AC BC Until C tower is complete
   } else {
+    for (let i = 0, j = 0; i < size; i++, j++) {
+      console.log(moves2[j]);
+      if (j == 2) j = 0;
+    }
     //algorith 1 AC AB BC Until C tower is complete
   }
 }
