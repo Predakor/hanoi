1346c64a7f1ca32d3537986ecfde11e85a065494
Predakor <86599904+Predakor@users.noreply.github.com>
2021-10-29 22:52:03 +0200

    history registration complete + html rewrite

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const towers = document.getElementsByClassName("tower");
 const counter = document.getElementById("counter");
+const historyPanel = document.querySelector(".history-window");
 const colors = [
   "#f72585",
   "#b5179e",
@@ -44,6 +45,9 @@ function reset() {
   currentBlock = 0;
   moves = 0;
   updateMovesCounter();
+  while (historyPanel.firstChild) {
+    historyPanel.removeChild(historyPanel.firstChild);
+  }
   for (let i = 0; i < size; i++) {
     towers[0].appendChild(blocks[i]);
   }
@@ -52,10 +56,7 @@ function reset() {
 function change() {
   num = parseInt(hanoiNum.value);
 
-  if (num == size) {
-    reset();
-    return;
-  }
+  if (num == size) return;
 
   //desired size < actual size
   if (num < size) {
@@ -79,40 +80,55 @@ function grabBlock() {
 }
 
 function check() {
-  var firstBlock = this.firstChild;
+  let firstBlock = this.firstChild;
   if (currentBlock != 0)
-    if (firstBlock == null) moveBlock(this);
-    else if (currentBlock.style.width < firstBlock.style.width) moveBlock(this);
+    if (firstBlock == null || currentBlock.style.width < firstBlock.style.width)
+      moveBlock(this);
 
   function moveBlock(target) {
+    let startTower = towerToLetter(currentBlock.parentElement);
     target.prepend(currentBlock);
     moves++;
     currentBlock = "";
     counter.innerText = moves + "/" + minMoves;
+
+    addMove(startTower, towerToLetter(target));
     if (towers[2].childNodes.length == size) alert("You win");
   }
 }
 
+function addMove(_startTower, _endTower) {
+  let currentMove = document.createElement("div");
+  currentMove.innerText = `${moves}. ${_startTower}>${_endTower}`;
+  currentMove.classList.add("history-row");
+  historyPanel.appendChild(currentMove);
+}
 function updateMovesCounter() {
   minMoves = 2 ** size - 1;
   counter.innerText = "0/" + minMoves;
+}
+function towerToLetter(_tower) {
+  if (_tower == towers[0]) return "A";
+  if (_tower == towers[1]) return "B";
+  if (_tower == towers[2]) return "C";
 }
 //=================solution section======================//
 function solution() {
   min = size * size - 1;
   let moves = ["AB", "AC", "BC"];
   let moves2 = ["AC", "AB", "BC"];
+  let j = 0;
   if (size % 2 == 0) {
-    for (let i = 0, j = 0; i < min; i++) {
-      j = j > 2 ? (j = 0) : j++;
+    //algorith  AB AC BC Until C tower is complete
+    for (let i = 0; i < min; i++, j++) {
+      if (j == 3) j = 0;
       console.log(moves[j]);
     }
-    //algorith  AB AC BC Until C tower is complete
   } else {
-    for (let i = 0, j = 0; i < min; i++, j++) {
-      j = j > 2 ? (j = 0) : j++;
+    //algorith  AC AB BC Until C tower is complete
+    for (let i = 0; i < min; i++, j++) {
+      if (j == 3) j = 0;
       console.log(moves2[j]);
     }
-    //algorith  AC AB BC Until C tower is complete
   }
 }
